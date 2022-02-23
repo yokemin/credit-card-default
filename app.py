17da75f33634c1986523e09466bf68078efff911
Yoke Min <46562387+yokemin@users.noreply.github.com>
2022-02-23 16:10:18 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+from flask import request, render_template
+from keras.models import load_model
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method=="POST":
+        income = request.form.get("income")
+        age = request.form.get("age")
+        loan = request.form.get("loan")
+        print(income, age, loan)
+        
+        model = load_model("default_model")
+        pred = model.predict([[float(income), float(age), float(loan)]])
+        s = "The predicted default score is " + str(pred)
+        return(render_template("index.html", result=s))
+    else:
+        return(render_template("index.html", result="2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
